c042279c62bb9c08b5985e40d34a73833538303b
Majd Arkilo <35635821+MajdArkilo@users.noreply.github.com>
2018-11-06 19:24:44 -0500

    refactoring

--- temp.c
+++ temp.c
@@ -15,140 +15,112 @@ int main(int argc,char *argv[])
     int len,newTemp,oldTemp,absTemp,highestTemp,lowestTemp;
     FILE *fp;
     char path[PATH_MAX];
-	 if(argc > 3)
-    {
-        printf("Please enter only one argument");
-        return 1;
-    }
+	if(argc > 3)
+        {
+            printf("Please enter only one argument");
+            return 1;
+        }
     printf("%d\n", argc);
     printf("the parameter enterted %s\n", argv[1]);
-strcpy(serialDir,"/sys/bus/w1/devices/28-");
-strcat(serialDir, argv[1]);
-strcat(serialDir,"/w1_slave");
-printf("%s\n",serialDir);
-strcpy(serialNum,"/sys/bus/w1/devices/28-021312eefeaa/w1_slave");
-oldTemp = -10;
-while(1)
-{  
-    fp = fopen(serialDir, "r");
-    
-    if (fp == NULL)
-    {   
-        printf("temperature sensor cannot be found\n");
-        return 2;
-    }   
-
-    while (fgets(path, PATH_MAX, fp) != NULL)
-    {   
-        printf("%s", path);
-        ret = strpbrk(path, "Yes");
-        if(ret)
-                {
-                        printf("Sensor working, will print the temp\n");
-                }
-    
-    }   
-    
-    len = strlen(path)- 6;
-    s1 = path+len;
-/* printf("%d\n",len);
-printf("%s\n",s1); */
-strcpy(holdval,s1);
-newTemp = atoi(holdval);
-
-printf("this is the old value %d\n", oldTemp);
-printf("this is the new value %d\n\n", newTemp);
-
-fclose(fp);
-if((newTemp < 0 && oldTemp > 0))
-{
-	printf("we see a change 20 \n");
-	oldTemp = newTemp;
-	if(newTemp > highestTemp)
-		{
-			highestTemp = newTemp;
-		}
-	if(newTemp < lowestTemp)
-		{
-			lowestTemp = newTemp;
-		}
-	ifttt("https://maker.ifttt.com/trigger/TempChange/with/key/uQayuY3amwBonQr7RGtpk", highestTemp, newTemp, lowestTemp);
-}
-if((newTemp > 0 && oldTemp < 0))
-{
-	 printf("we see a change 10\n");
-	oldTemp = newTemp;
-	if(newTemp > highestTemp)
-                {
-                        highestTemp = newTemp;
-                }
-        if(newTemp < lowestTemp)
-                {
-                        lowestTemp = newTemp;
-                }
-
-	ifttt("https://maker.ifttt.com/trigger/TempChange/with/key/uQayuY3amwBonQr7RGtpk", highestTemp, newTemp, lowestTemp);
-}
-if(oldTemp > 0 && newTemp > 0)
-{
-	
-	absTemp = abs(oldTemp - newTemp);
-	printf("both of them are positive\n");
-	if(absTemp >= 1000)
-	{
-		printf("Change in temp, both were positive\n");
-		if(newTemp > highestTemp)
-                {
-                        highestTemp = newTemp;
-                }
-	        if(newTemp < lowestTemp)
-                {
-                        lowestTemp = newTemp;
-                }
-
-		ifttt("https://maker.ifttt.com/trigger/AlarmTriggerd3/with/key/uQayuY3amwBonQr7RGtpk", "Temp", "is", "changing");
-	}
-	
-
-}
-else
-{
-	absTemp = abs(abs(oldTemp)-abs(newTemp));
-	printf("both are negative\n");
-	if(absTemp >= 1000)
-        {
-                printf("Change in temp, both were negative\n");
-		if(newTemp > highestTemp)
-                {
-                        highestTemp = newTemp;
-                }
-	        if(newTemp < lowestTemp)
-                {
-                        lowestTemp = newTemp;
-                }
-			ifttt("https://maker.ifttt.com/trigger/AlarmTriggerd3/with/key/uQayuY3amwBonQr7RGtpk", "Temp", "is", "changing");
-        }
-}
-
-
-}
-/*
-    printf("%c\n",path[len]);
-    printf("%c\n",path[len+1]);
-    printf("%c\n",path[len+2]);
-    printf("%c\n",path[len+3]);
-    printf("%c\n",path[len+4]);
-*/
-/*
-    currentChar = path[len];
-   val = atof(currentChar);
-   val = val *10;
-   currentChar = path[len
-*/
+    strcpy(serialDir,"/sys/bus/w1/devices/28-");
+    strcat(serialDir, argv[1]);
+    strcat(serialDir,"/w1_slave");
+    printf("%s\n",serialDir);
+    strcpy(serialNum,"/sys/bus/w1/devices/28-021312eefeaa/w1_slave");
+    oldTemp = -10;
+    while(1)
+    {  
+        fp = fopen(serialDir, "r");
+        if (fp == NULL)
+            {   
+                printf("temperature sensor cannot be found\n");
+                return 2;
+            }   
+        while (fgets(path, PATH_MAX, fp) != NULL)
+            {   
+                printf("%s", path);
+                ret = strpbrk(path, "Yes");
+                if(ret)
+                        {
+                                printf("Sensor working, will print the temp\n");
+                        }
+            }   
+        
+        len = strlen(path)- 6;
+        s1 = path+len;
+        strcpy(holdval,s1);
+        newTemp = atoi(holdval);
+        printf("this is the old value %d\n", oldTemp);
+        printf("this is the new value %d\n\n", newTemp);
+        fclose(fp);
+        if((newTemp < 0 && oldTemp > 0))
+            {
+            	printf("we see a change 20 \n");
+            	oldTemp = newTemp;
+            	if(newTemp > highestTemp)
+            		{
+            			highestTemp = newTemp;
+            		}
+            	if(newTemp < lowestTemp)
+            		{
+            			lowestTemp = newTemp;
+            		}
+            	ifttt("https://maker.ifttt.com/trigger/TempChange/with/key/uQayuY3amwBonQr7RGtpk", highestTemp, newTemp, lowestTemp);
+            }
+        if((newTemp > 0 && oldTemp < 0))
+            {
+            	printf("we see a change 10\n");
+            	oldTemp = newTemp;
+            	if(newTemp > highestTemp)
+                            {
+                                    highestTemp = newTemp;
+                            }
+                if(newTemp < lowestTemp)
+                            {
+                                    lowestTemp = newTemp;
+                            }
+            	ifttt("https://maker.ifttt.com/trigger/TempChange/with/key/uQayuY3amwBonQr7RGtpk", highestTemp, newTemp, lowestTemp);
+            }
+        if(oldTemp > 0 && newTemp > 0)
+            {
+            	absTemp = abs(oldTemp - newTemp);
+            	printf("both of them are positive\n");
+            	if(absTemp >= 1000)
+            	{
+            		printf("Change in temp, both were positive\n");
+            		if(newTemp > highestTemp)
+                            {
+                                    highestTemp = newTemp;
+                            }
+            	        if(newTemp < lowestTemp)
+                            {
+                                    lowestTemp = newTemp;
+                            }
+            		ifttt("https://maker.ifttt.com/trigger/AlarmTriggerd3/with/key/uQayuY3amwBonQr7RGtpk", "Temp", "is", "changing");
+            	}
+            }
+        else
+            {
+            	absTemp = abs(abs(oldTemp)-abs(newTemp));
+            	printf("both are negative\n");
+            	if(absTemp >= 1000)
+                    {
+                        printf("Change in temp, both were negative\n");
+            		    if(newTemp > highestTemp)
+                            {
+                                    highestTemp = newTemp;
+                            }
+            	        if(newTemp < lowestTemp)
+                            {
+                                    lowestTemp = newTemp;
+                            }
+            			ifttt("https://maker.ifttt.com/trigger/AlarmTriggerd3/with/key/uQayuY3amwBonQr7RGtpk", "Temp", "is", "changing");
+                    }
+            }
+    }
 
 return 0;
 }
-
 
 
 
